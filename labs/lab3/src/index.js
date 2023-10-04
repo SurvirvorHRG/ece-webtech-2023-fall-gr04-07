5b89f2302da7a0dd7d43bffa16144f3b09a9f85e
SurvirvorHRG <ti-yoyo789@hotmail.fr>
2023-10-04 12:49:16 +0200

    fix: routes

--- labs/lab3/src/index.js
+++ labs/lab3/src/index.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-const db = require('./dbClient')
+/*const db = require('./dbClient')
 db.on("error", (err) => {
   console.error(err)
-})
+})*/
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use('/', userRouter)
+app.use('/', userHandles)
 
 const server = app.listen(port, (err) => {
   if (err) throw err
--- labs/lab3/src/routes/handles.js
+++ labs/lab3/src/routes/handles.js
@@ -2,8 +2,7 @@
 const url = require('url')
 const qs = require('querystring')
 const fs = require('fs')
-const about = require('./content/about')
-
+const about = require('../../content/about')
 const express = require('express')
 
 const userHandles = express.Router()
@@ -20,19 +19,20 @@ userHandles
     '    <body>' +
     '       <p>Hello !</p>' +
     '       <p>If you want to get a customized hello message follow theses instructions: </p>' +
-    '       <p>Go to <a href="hello">/hello</a> and specify any name you want as query parameters by adding ?name="NAME" to the end of the url !! </p>' +
+    '       <p>Go to <a href="hello">/hello</a> and specify any name you want as parameters by adding /"NAME" to the end of the url !! </p>' +
     '    </body>' +
     '</html>'
 
-    resp.set(200, {'Content-Type': 'text/html'})
+    resp.status(200).set('Content-Type','text/html')
     resp.send(content)
+
 
   })
 
   .get('/hello', (req, resp, next) => { 
   
-    res.set(200, {'Content-Type': 'text/html'})
-    res.send("To see a Hello message: /hello?name='NAME' !" + '<br/>')
+    resp.status(200).set('Content-Type','text/html')
+    resp.send("To see a Hello message: /hello/:name !" + '<br/>')
 
   })
 
@@ -40,23 +40,25 @@ userHandles
   
     const name = req.params.name
 
-    resp.set(200, {'Content-Type': 'text/html'})
+    resp.status(200).set('Content-Type','text/html')
 
     if(name === 'Erwan')
     {
       resp.send("Hello ! I'm Erwan an engineering student at ECE Paris !")
     }
     else
-      resp.send('Hello ' + params['name'])
+      resp.send('Hello ' + name)
 
   })
 
   .get('/about', (req, resp, next) => { 
   
-    resp.set("Content-Type", "application/json")
+    resp.status(200).set('Content-Type','application/json')
+    let content = "";
     for(var attributename in about){
-      resp.send(attributename+": "+ about[attributename] + '\n');
+      content += attributename+": "+ about[attributename] + '\n';
     }
+    resp.send(content)
 
     
 
@@ -65,23 +67,26 @@ userHandles
   .get('/*/', (req, resp, next) => { 
 
     let path = req.path
-    if(fs.existsSync("./content" + path + '.json') )
+    if(fs.existsSync('test.json') )
     {
-      let json = require("./content" + path)
-      res.set("Content-Type", "application/json");
+      let json = require('test')
+      resp.set('Content-Type','application/json');
+      let content = "";
       for(var attributename in json){
-        res.send(attributename+": "+json[attributename] + '\n');
+        content += attributename+": "+ about[attributename] + '\n';
       }
+      resp.send(content)
+  
     }
     else
     {
-      res.set(200, {'Content-Type': 'text/html'})
-      res.send('ERROR 404: NOT FOUND')
+      resp.status(200).set('Content-Type','text/html')
+      resp.send('ERROR 404: NOT FOUND' + " " + path)
     }
 
   })
   
-module.exports = userRouter
+module.exports = userHandles
 
 
 /*module.exports = {
