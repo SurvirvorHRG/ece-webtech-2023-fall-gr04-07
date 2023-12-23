184ceb5a3e4e6806bcc0d7035ae18cbe7ea8d7c7
SurvirvorHRG <exwan789@gmail.com>
2023-12-23 14:32:17 +0100

    feat: write article page

--- app/components/cardList/CardList.js
+++ app/components/cardList/CardList.js
@@ -2,15 +2,12 @@ import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination.js";
 import Image from "next/image";
-import Card from "../card/Card.js";
 
 
 
 const CardList = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Recent Posts</h1>
-      <div className={styles.posts}></div>
+    <div className={styles.container}>CardList
       <Pagination/>
     </div>
   );
--- app/components/pagination/Pagination.js
+++ app/components/pagination/Pagination.js
@@ -1,30 +1,14 @@
-"use client";
+"use client"
 
-import React from "react";
-import styles from "./pagination.module.css";
-import { useRouter } from "next/navigation";
+import React from "react"
+import styles from "./pagination.module.css"
+import { useRouter } from "next/navigation"
 
-const Pagination = ({ page, hasPrev, hasNext }) => {
-  const router = useRouter();
+const Pagination = () => {
 
   return (
-    <div className={styles.container}>
-      <button
-        className={styles.button}
-        disabled={!hasPrev}
-        onClick={() => router.push(`?page=${page - 1}`)}
-      >
-        Previous
-      </button>
-      <button
-        disabled={!hasNext}
-        className={styles.button}
-        onClick={() => router.push(`?page=${page + 1}`)}
-      >
-        Next
-      </button>
-    </div>
-  );
-};
+    <div className={styles.container}>Pagination</div>
+  )
+}
 
-export default Pagination;
+export default Pagination
